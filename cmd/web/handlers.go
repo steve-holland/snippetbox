c47505461ba6a56fb7bcf9f8c201f2ef0add5d52
Steve Holland <steve@codebysubtract.com>
2023-05-17 15:55:03 +0100

    Adds About page.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -46,6 +46,11 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "home.tmpl", data)
 }
 
+func (app *application) about(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "about.tmpl", data)
+}
+
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	params := httprouter.ParamsFromContext(r.Context())
 
